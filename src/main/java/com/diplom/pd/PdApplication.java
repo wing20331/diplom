86c3e86ab4668eb85d8c19444835e88b128ce7af
wing20331 <wing20331@gmail.com>
2024-01-08 13:03:38 +0300

    start

---
+++ src/main/java/com/diplom/pd/PdApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.pd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplom/pd/PdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.pd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
